c90e1daa42716fb8634b553f3a9c5fdb167adbcc
thaherster <thaherster@gmail.com>
2019-05-16 23:43:32 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const experss = require("express");
+
+const app = experss();
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server Started on Port" + PORT);
+});
